e9316c7d77c2170b3cb1b38dab82a02af95e847c
Asnake Assefa <30431907+asnakeassefa@users.noreply.github.com>
2023-02-23 15:59:06 +0300

    Create minimum-size-subarray-sum.py
    This is the solution to problem : https://leetcode.com/problems/minimum-size-subarray-sum/

---
+++ minimum-size-subarray-sum.py
@@ -0,0 +1,16 @@
+ptr1 = 0
+        ptr2 = 0
+        length = len(nums)
+        Sum = 0
+        ans = float('inf')
+        
+        while ptr2 < length:
+            Sum += nums[ptr2]
+            while Sum >= target:
+                ans = min(ans,ptr2-ptr1+1)
+                Sum -= nums[ptr1]
+                ptr1 += 1
+            ptr2 += 1
+        if ans == float('inf'):
+            return 0
+        return ans
